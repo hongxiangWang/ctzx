1eabf3c47e6fafa29752794af827aa4f23ff98b1
zhangwentao <1512168499@qq.com>
2017-09-29 12:03:43 +0800

    zwt 1

--- src/page/approve/steps.vue
+++ src/page/approve/steps.vue
@@ -1,14 +1,16 @@
 <template>
     <div id="steps">
-        {{stepData}}
+        <!--{{stepData}}-->
+
+        {{rowData}}
         <el-steps direction="vertical" :space="100" :active="stepData.active">
             <el-step v-for="(item,index) in stepData.steps"
                      :title="item.title"
                      :status="item.status">
                 <span slot="description">
                     {{item.description}}
-                    <el-button type="text" @click="pass" v-if="stepData.passBtn">通过</el-button>
-                    <el-button type="text" @click="pass" v-if="stepData.backBtn">退回</el-button>
+                    <el-button type="text" @click="pass_agree" v-if="item.passBtn">通过</el-button>
+                    <el-button type="text" @click="pass_back" v-if="item.backBtn">退回</el-button>
                 </span>
             </el-step>
         </el-steps>
@@ -26,11 +28,15 @@
             }
         },
         props:{
-            stepData:{}
+            stepData:{},
+            rowData:{}
         },
         methods: {
-            pass(){
-                this.$emit('stepPass');
+            pass_agree(){
+                this.$emit('stepPass',1);//确定按钮
+            },
+            pass_back(){
+                this.$emit('stepPass',0);//取消按钮
             },
             handleClose(){
                 this.$emit('stepEditClose');
